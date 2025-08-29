7d5a9c2c466a3acfc9f4403b4d6a49ffe174e06d
Tang Zhi Xiang <51744163+DarkRelicz@users.noreply.github.com>
2025-08-30 01:44:18 +0800

    Revert "Deleted redundencies"

--- view/mittens/src/components/nav/top-nav.tsx
+++ view/mittens/src/components/nav/top-nav.tsx
@@ -6,7 +6,7 @@ import { ThemeToggle } from "../theme-toggle";
 export default function TopNav({ title }: { title: string }) {
   return (
     <Container className="flex h-16 items-center justify-between border-b border-border">
-      <h1 className="text-2xl font-medium text-black dark:text-white">{title}</h1>
+      <h1 className="text-2xl font-medium">{title}</h1>
       <ThemeToggle />
     </Container>
   );
